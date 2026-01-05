5fefff7283168f9f7d925a321e1e1c8ed8800d8b
eeekeee <ekrtionl@gmail.com>
2026-01-05 20:33:10 +0900

    feat: RedisKey 설정 (#59)

--- apps/api/src/redis/redis-key.constant.ts
+++ apps/api/src/redis/redis-key.constant.ts
@@ -12,4 +12,18 @@ export class RedisKeys {
   static room(roomId: string): string {
     return `room:${roomId}:info`;
   }
+
+  // Matching 관련
+
+  // ZSET: 매칭 대기 큐 (score = rating)
+  static matchingQueue(): string {
+    return `matching:queue`;
+  }
+
+  // HSET: 매칭 대기 사용자 정보
+  static matchingUser(userId: string): string {
+    return `matching:user:${userId}`;
+  }
+
+  //
 }
