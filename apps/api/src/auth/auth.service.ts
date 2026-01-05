d4ce1cecddcadd31f43a65bfc9a599a74cb26de8
dyeon-dev <kdy37912@naver.com>
2026-01-05 18:59:48 +0900

    feat: 로그인 성공 시 jwt 토큰 생성 (#57)
    - jwtService.sign 메서드를 사용하면 Secret + Payload을 합쳐서 access token 생성
    - 토큰으로 정보읽기가 가능하기 때문에 payload에는 중요한 정보는 넣지 않기 위해 username, id만 추가

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
 
 @Injectable()
@@ -9,4 +10,12 @@ export class AuthService {
     private userService: UserService,
     private jwtService: JwtService,
   ) {}
+
+  // 로그인 성공 시 토큰 생성 (Secret + Payload)
+  login(user: User) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
 }
