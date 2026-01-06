1f0cf41fbc83a0fca542270eea3e8f083cd5327e
dyeon-dev <kdy37912@naver.com>
2026-01-06 14:18:31 +0900

    feat(fe): 로그인 만료 시 에러 페이지 노출 (#57)
    - /error 라우팅으로 LoginErrorPage 렌더링
    - 401 에러 발생 시 에러 페이지로 리다이렉트

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from '@/App.tsx';
 import BattlePage from '@/pages/BattlePage.tsx';
+import LoginErrorPage from '@/pages/Error/LoginErrorPage';
 import LoginPage from '@/pages/LoginPage.tsx';
 import MainPage from '@/pages/MainPage.tsx';
 
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: '/room/:roomId',
         element: <BattlePage />,
+      },
+      {
+        path: '/error',
+        element: <LoginErrorPage />,
       },
     ],
   },
