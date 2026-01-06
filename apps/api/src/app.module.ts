15cbe906bb1a82a341a837ecd8f1d69a55ebab97
dyeon-dev <kdy37912@naver.com>
2026-01-06 23:49:54 +0900

    chore: App Controller 모듈 삭제 (#57)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { BattleModule } from './battle/battle.module';
 import { RedisModule } from './redis/redis.module';
@@ -47,7 +46,7 @@ import { UserModule } from './user/user.module';
     UserModule,
     AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
