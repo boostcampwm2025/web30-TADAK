e97c5ca8751791303ceb2fc10245f2142932443c
Junsub Lee <lee980605@knu.ac.kr>
2026-01-14 18:07:44 +0900

    feat: 도커 constant 추가 (#115) - 러너 이미지명(judge-javascript) - 볼륨명(judge-problems, judge-submissions) - 리소스 제한 기본값 정의

---
+++ apps/judge/src/docker/docker.constants.ts
@@ -0,0 +1,8 @@
+export const DOCKER_RUNNER_IMAGE = 'judge-javascript';
+export const DOCKER_PROBLEMS_VOLUME = 'judge-problems';
+export const DOCKER_SUBMISSIONS_VOLUME = 'judge-submissions';
+
+export const DOCKER_CPU_LIMIT = 0.5;
+export const DOCKER_PIDS_LIMIT = 10;
+export const DOCKER_TMPFS_SIZE_MB = 32;
+export const DOCKER_DEFAULT_MEMORY_LIMIT_MB = 256;
