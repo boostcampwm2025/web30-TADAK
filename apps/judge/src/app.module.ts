cf48de5ae904242b67d4f6d2129220d470c23375
cchaeyoung <kimchaeyoung05@naver.com>
2026-01-15 16:50:39 +0900

    feat: TypeORM 엔티티 자동 로드 설정 (#122)

--- apps/judge/src/app.module.ts
+++ apps/judge/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { PubSubModule } from './pubsub/pubsub.module';
 import { RedisModule } from './redis/redis.module';
 import { SubmissionModule } from './submission/submission.module';
-import { PubSubModule } from './pubsub/pubsub.module';
 
 @Module({
   imports: [
@@ -24,13 +24,12 @@ import { PubSubModule } from './pubsub/pubsub.module';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_DATABASE'),
-        entities: [],
-        synchronize: true,
+        entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+        synchronize: false,
         logging: ['error'],
       }),
       inject: [ConfigService],
     }),
-    
 
     // Redis 연결 설정
     RedisModule,
