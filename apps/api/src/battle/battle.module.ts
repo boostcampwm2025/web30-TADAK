a6d481e70e18c39ae126a16cf3e041e4285c7a3a
dyeon-dev <kdy37912@naver.com>
2026-01-22 16:08:19 +0900

    fix: 순환 참조 문제 해결 (#181)
    - API 서버가 순환 참조로 인해 무한 재시작 상태 발생
    - 순환 참조 관계에 있는 RoomModule, BattleModule 양쪽에 forwardRef()를 적용해야 NestJS가 정상적으로 초기화
    - BattleModule이 RoomModule을 참조
    - RoomModule이BattleModule을 참조

--- apps/api/src/battle/battle.module.ts
+++ apps/api/src/battle/battle.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BattleController } from '@/battle/battle.controller';
@@ -7,11 +7,16 @@ import { BattleGateway } from '@/battle/battle.gateway';
 import { BattleService } from '@/battle/battle.service';
 import { BattleRedisService } from '@/battle/battle-redis.service';
 import { ProblemModule } from '@/problem/problem.module';
+import { RoomModule } from '@/room/room.module';
 import { Submission } from '@/submission/submission.entity';
 import { User } from '@/user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Battle, Submission, User]), ProblemModule],
+  imports: [
+    TypeOrmModule.forFeature([Battle, Submission, User]),
+    ProblemModule,
+    forwardRef(() => RoomModule),
+  ],
   controllers: [BattleController],
   providers: [BattleService, BattleRedisService, BattleGateway],
   exports: [BattleService, BattleGateway],
