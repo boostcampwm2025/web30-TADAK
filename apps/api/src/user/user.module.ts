57b58b8b760173876ff3c79de674bdbafd06e97d
cchaeyoung <kimchaeyoung05@naver.com>
2026-01-29 16:31:22 +0900

    feat: 배틀 결과 조회를 위한 관련 엔티티 등록 (#231)
    - UserModule에 Battle, Submission, Problem 엔티티 주입

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Battle } from '@/battle/battle.entity';
+import { Problem } from '@/problem/problem.entity';
+import { Submission } from '@/submission/submission.entity';
+
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Battle, Submission, Problem])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
